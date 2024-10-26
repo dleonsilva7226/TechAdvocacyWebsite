4206e971e42bf988de9935c96de68df59f5afb8c
dleonsilva7226 <dleonsilva7226@gmail.com>
2024-10-25 21:40:54 -0400

    Implement styling and organization of the advocacy website

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ let isDarkMode = false;
 
 function changeBackgroundInverse () {
     if (isDarkMode) {
-        document.querySelector("body").style.backgroundColor = "#B5DBFB";
-        document.querySelector("body").style.color = "black";
-        darkModeButton.innerHTML = "Toggle Dark Mode";
+        document.querySelector("body").style.backgroundColor = "#284574";
+        document.querySelector(".header-section").style.backgroundColor = "#edc967";
+        // document.querySelector("body").style.color = "black";
+        darkModeButton.innerHTML = "Dark Mode";
         isDarkMode = false;
     } else {
-        document.querySelector("body").style.backgroundColor = "#4A4A4A";
-        document.querySelector("body").style.color = "white";
-        darkModeButton.innerHTML = "Toggle Light Mode";
+        document.querySelector("body").style.backgroundColor = "rgb(30,30,30)";
+        // document.querySelector("body").style.color = "white";
+        document.querySelector(".header-section").style.backgroundColor = "#4A4A4A";
+        darkModeButton.innerHTML = "Light Mode";
+        
         isDarkMode = true;
     }
 }
