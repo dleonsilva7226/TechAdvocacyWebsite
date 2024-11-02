5c75f81eefa55c7ffc8165ab9fae82544619d25d
dleonsilva7226 <dleonsilva7226@gmail.com>
2024-11-02 16:19:06 -0400

    Implement Unit 6 Requirements to Website and Working to Implement Corrections on Website

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ function changeBackgroundInverse () {
 }
 
 darkModeButton.addEventListener("click", changeBackgroundInverse);
+
+
+
+
+//PETITION SIGNING SECTION
+
+// Add your query for the sign now button here
+let signNowButton = document.getElementById("sign-now-button");
+const addSignature = () => {
+    // Write your code to manipulate the DOM here
+    let userFirstName = document.getElementById("fname").value;
+    let userHometown = document.getElementById("hometown").value;
+    let newSignature = document.createElement("p");
+    newSignature.innerHTML = "🖊️ " + userFirstName + " from " + userHometown + " supports this."
+    document.querySelector(".signatures").appendChild(newSignature);
+}
+
+// Add a click event listener to the sign now button here
+signNowButton.addEventListener("click", addSignature);
