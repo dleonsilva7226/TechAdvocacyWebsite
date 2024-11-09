60db2bcf51d5a6de32eb2c286a21ed560e671703
dleonsilva7226 <dleonsilva7226@gmail.com>
2024-11-08 22:33:17 -0500

    Implement Petition Section of Website

--- script.js
+++ script.js
@@ -38,4 +38,41 @@ const addSignature = () => {
 }
 
 // Add a click event listener to the sign now button here
-signNowButton.addEventListener("click", addSignature);
+// signNowButton.addEventListener("click", addSignature); //DELETE THIS
+
+// TODO: Remove the click event listener that calls addSignature()
+
+// TODO: Complete validation form
+
+const validateForm = () => {
+
+    let containsErrors = false;
+  
+    var petitionInputs = document.getElementById("sign-petition").elements;
+    // TODO: Loop through all inputs
+    for (let i = 0; i < petitionInputs.length; i++) {
+
+        // TODO: Validate the value of each input
+        if (petitionInputs[i].value.length < 2) { 
+            containsErrors = true;
+            petitionInputs[i].classList.add('error');
+        } else {
+            petitionInputs[i].classList.remove('error');
+        }
+    }
+
+    if (!containsErrors) {
+        addSignature();
+        for (let i = 0; i < petitionInputs.length; i++) {
+            petitionInputs[i].value = "";
+            containsErrors = false;
+        }
+    }
+
+  
+  
+    // TODO: Call addSignature() and clear fields if no errors
+  
+  }
+  
+  signNowButton.addEventListener('click', validateForm);
